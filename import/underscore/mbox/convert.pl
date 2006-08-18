f4775295015ac104d2af061b46620608f521cbc5
Dan Brickley <danbri@users.noreply.github.com>
2006-08-18 17:15:12 +0000

    rdf-happy version

--- import/underscore/mbox/convert.pl
+++ import/underscore/mbox/convert.pl
@@ -1,26 +1,43 @@
+#!/usr/bin/perl
 
-stalking _ members...
+use Mail::Box::Mbox;
+use Data::Dumper;
+use Digest::SHA1 qw(sha1_hex);
 
-existing foaf:
-http://gidden.net/tom/foaf.rdf
+my $folder = Mail::Box::Mbox->new(folder => 'underscore.mbox');
 
+my @messages   = $folder->messages;
 
+my $rdf='<rdf:RDF xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:foaf="http://xmlns.com/foaf/0.1/" xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#">';
+ 
 
+foreach my $msg (@messages) {
 
-stalking:
+	my $time = $msg->guessTimestamp();
+	my @fromarr = $msg->from();
+	my $from = $fromarr[0];
+	my $subj = $msg->subject();
+#	print "$time\t$subj\t".$from->name."\t".$from->address."\n";
+       $rdf .= rdfize($time,$subj,$from->name, $from->address) ;
+}
 
+sub rdfize($,$,$,$) {
+ my ($time, $subj, $who, $email) =@_;
+	
+my $sha1sum = sha1_hex("mailto:".$email);
+ 
+my $post= "\n\n<foaf:Document><dc:title>$subj</dc:title>\n";
+$post .= "   <foaf:maker><foaf:Person><foaf:name>$who</foaf:name>\n";
+$post .= "   <foaf:mbox rdf:resource='mailto:$email'/>\n";
+$post .= "   <foaf:mbox_sha1sum>$sha1sum</foaf:mbox_sha1sum>\n";
+$post .= "  </foaf:Person>\n";
+$post .= " </foaf:maker>\n";
+$post .= "</foaf:Document>\n\n\n";
 
+ return $post;
+}
 
+$rdf .= "</rdf:RDF>\n\n";
 
-
-http://www.davehodgkinson.com/index.shtml
-http://www.flickr.com/people/davehodg/
-Dave Hodgkinson	mailto:davehodg@gmail.com
-
-http://petef.com/
-
-
-Andrew Parkhouse
-http://tdb.berlinale-talentcampus.de/campus/talent/andrew-parkhouse/profile?pos=151
-
+print $rdf;
 
