50ce0a9fa225efe104c07a364bc194a0107a3e30
gid <gid@55bc8a7a-491b-0410-89ea-f8cff589e9ef>
2006-08-19 19:19:40 +0000

    Rewrote using RDF::Helper API.  The output probably isn't correct, especially with regards to the IDs used.

--- import/underscore/mbox/convert.pl
+++ import/underscore/mbox/convert.pl
@@ -1,43 +1,84 @@
 #!/usr/bin/perl
 
-use strict;
 
-use Mail::Box::Mbox;
+# Pick an example... any example:
+
+my $input_fn = 'test-files/august-mails.mbox';
+#my $input_fn = 'test-files/2006-August.txt';
+#my $input_fn = 'test-files/sample.txt');
+
+
+use Mail::Box::Manager;
 use Data::Dumper;
-use rdfize;
+use foafize;
 use underscore;
 
-#my $folder = Mail::Box::Mbox->new(folder => 'test-files/august-mails.mbox');
-#my $folder = Mail::Box::Mbox->new(folder => 'test-files/sample.txt');
-my $folder = Mail::Box::Mbox->new(folder => 'test-files/2006-August.txt');
+use strict;
+use warnings;
+
+# 'foafize' is a temporary module for converting Mail::Box::Message
+# objects into RDF.  It's a subclass of RDF::Helper.
+#
+# You can feed foafize::new() the normal RDF::Helper parameters:
+#    our $rdfstorage = new RDF::Redland::Storage("hashes", "test",
+#                                  "new='yes',hash-type='memory'");
+#    our $rdfmodel = new RDF::Redland::Model($rdfstorage,'');
+#    $foafize = new foafize(Model => $rdfmodel);
+#
+# Note, it's not *actually* a subclass of RDF::Helper at the moment: see
+# the comments inside that module.
+my $foafize = new foafize();
+
+
+# Start a mailbox manager.  This is needed to allow threading.  It also
+# adds a little bit of portability.
+my $mgr = new Mail::Box::Manager;
+
+
+# Load the input file
+my $folder = $mgr->open(folder => $input_fn);
 
 
 # Un-spam-protect the email addresses.  Only necessary if using raw files
 # from http://www.under-score.org.uk/
 underscore_unmunge($folder);
 
-# Get all messages from the archive
-my @messages = $folder->messages;
 
-# Buffering in an array followed by a single array join is faster than
-# bigass string concatenation when dealing with loads of data.
-my @rdfbuf;
+# Get all threads from the archive
+my $threads = $mgr->threads($folder);
 
-# Add in a generic RDF header
-push @rdfbuf, rdfheader();
 
-# For each message...
-foreach my $msg (@messages) {
-	# RDF it.
-	push @rdfbuf, rdfize($msg);
+# For each initial (non-reply) message (ie. each thread start)...
+foreach my $m ($threads->all) {
+
+	# Recursively FOAF the message.
+	walk_thread($foafize, $m);
 }
 
-# Add the closing gubbins for RDF
-push @rdfbuf, rdffooter();
 
-# Output it.
-print join "\n", @rdfbuf;
-print "\n";
+# Output the RDF as XML to STDOUT.
+print $foafize->serialize()."\n";
+exit;
 
 
+sub walk_thread {
+# This takes a foafize object and a Mail::Box::Thread::Node and FOAFs the
+# node if possible.  It then recurses through all followups and FOAFs them
+# too.
 
+	my ($foafize, $m) = @_;
+
+	my $msg = $m->message;
+
+	# If this is a real message (as opposed to an unknown message that
+	# exists only because it's the parent to a message that _is_ known),
+	# then FOAF it.
+	unless($msg->isDummy) {
+		$foafize->foaf_message($m);
+	}
+
+	# FOAF the kids too.
+	foreach my $f ($m->followUps) {
+		walk_thread($foafize, $f);
+	}
+}
