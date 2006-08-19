96915f9e5e6df61ef5145cde694a9f0b5266fc58
Tom Gidden <tomgidden@users.noreply.github.com>
2006-08-19 10:55:32 +0000

    Reorganised a bit, so I can separate out the RDF'ing from the mail processing. I expect some of this stuff will get pulled out into a parent project (import, or import/underscore) for sharing with the other importers.
    I would also expect, in time, some of this stuff to end up in a FOAFTown
    perl package: FOAFTown::Import::Underscore, or somesuch.
    
    Sorry for munging code indiscriminately, btw.

--- import/underscore/mbox/convert.pl
+++ import/underscore/mbox/convert.pl
@@ -2,42 +2,28 @@
 
 use Mail::Box::Mbox;
 use Data::Dumper;
-use Digest::SHA1 qw(sha1_hex);
+use rdfize;
 
-my $folder = Mail::Box::Mbox->new(folder => 'underscore.mbox');
+my $folder = Mail::Box::Mbox->new(folder => 'test-files/august-mails.mbox');
+#my $folder = Mail::Box::Mbox->new(folder => 'test-files/2006-August.txt');
 
-my @messages   = $folder->messages;
+my @messages = $folder->messages;
 
-my $rdf='<rdf:RDF xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:foaf="http://xmlns.com/foaf/0.1/" xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#">';
- 
+my @rdfbuf;
+
+push @rdfbuf, '<rdf:RDF '.
+	'xmlns:dc="http://purl.org/dc/elements/1.1/" '.
+	'xmlns:foaf="http://xmlns.com/foaf/0.1/" '.
+	'xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"'.
+	'>';
 
 foreach my $msg (@messages) {
-
-	my $time = $msg->guessTimestamp();
-	my @fromarr = $msg->from();
-	my $from = $fromarr[0];
-	my $subj = $msg->subject();
-#	print "$time\t$subj\t".$from->name."\t".$from->address."\n";
-       $rdf .= rdfize($time,$subj,$from->name, $from->address) ;
+	push @rdfbuf, rdfize($msg);
 }
 
-sub rdfize($,$,$,$) {
- my ($time, $subj, $who, $email) =@_;
-	
-my $sha1sum = sha1_hex("mailto:".$email);
- 
-my $post= "\n\n<foaf:Document><dc:title>$subj</dc:title>\n";
-$post .= "   <foaf:maker><foaf:Person><foaf:name>$who</foaf:name>\n";
-$post .= "   <foaf:mbox rdf:resource='mailto:$email'/>\n";
-$post .= "   <foaf:mbox_sha1sum>$sha1sum</foaf:mbox_sha1sum>\n";
-$post .= "  </foaf:Person>\n";
-$post .= " </foaf:maker>\n";
-$post .= "</foaf:Document>\n\n\n";
+push @rdfbuf, "</rdf:RDF>\n\n";
 
- return $post;
-}
+print join "\n", @rdfbuf;
 
-$rdf .= "</rdf:RDF>\n\n";
 
-print $rdf;
 
