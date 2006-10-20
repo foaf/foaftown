4cf0cf5ec46cd6b11aa7c65fa7fca7f547da6dfe
danbri <danbri@55bc8a7a-491b-0410-89ea-f8cff589e9ef>
2006-10-20 09:05:33 +0000

    countless crappy improvements

--- geo/photos/pics2ge.rb
+++ geo/photos/pics2ge.rb
@@ -1,9 +1,18 @@
 #!/usr/bin/env ruby
-
 require 'rubygems'
 require 'json'
 
-pics = `cat f.js`
+#file = ARGV.pop
+file = '_f.js' 
+pics = `cat #{file}`
+
+def esc(str)
+   str = str.to_s.gsub("&", "&amp;")
+   str = str.gsub("\"", "&apos;")
+   str = str.gsub("\"", "&quot;")
+   str = str.gsub("<", "&lt;")
+   str.gsub(">", "&gt;")
+end
 
 res = JSON.parse pics
 
@@ -11,31 +20,39 @@ puts '<?xml version="1.0" encoding="UTF-8"?>'
 puts "\n"
 puts '<kml xmlns="http://earth.google.com/kml/2.0">'
 puts "\n"
-puts "<Folder><name>SemLife Test</name>\n"
-
-
+puts "<Folder><name>foaftown tester</name>\n"
 
 res["results"]["bindings"].each do |pic| 
-  id = pic['id']['value']
-  owner = pic['owner']['value']
-  secret = pic['secret']['value']
-  server = pic['server']['value']
-  title= pic['title']['value']
-  lat=pic['latitude']['value']
-  lon=pic['longitude']['value']
+
+  id = esc pic['id']['value']
+  owner = esc pic['owner']['value']
+  secret = esc pic['secret']['value']
+  server = esc pic['server']['value']
+  title = esc pic['title']['value']
+  lat = esc pic['latitude']['value'] if pic['latitude']
+  lon = pic['longitude']['value'] if pic['longitude']
 
   # See  http://www.flickr.com/services/api/misc.urls.html
   s_url = "http://static.flickr.com/" + server + "/" + id + "_" + secret + "_s.jpg"
+  m_url = "http://static.flickr.com/" + server + "/" + id + "_" + secret + "_m.jpg"
+  image_url = "http://static.flickr.com/" + server + "/" + id + "_" + secret + ".jpg"
+
   img_page = "http://www.flickr.com/photos/" + owner +"/" + id 
 
-  puts "#{title} #{id} #{lat} #{lon} #{s_url} #{img_page}"
+#  puts "#{title} #{id} #{lat} #{lon} #{s_url} #{img_page}"
 
 puts "<Folder>
-   <name>image placemark</name>
+   <name>#{title}</name>
    <visibility>1</visibility>
    <Placemark>
-     <description><![CDATA[ <img src='#{s_url}' /> $media placemark, by $who at lat:#{lat} lon:#{lon} ]]></description>
-      <name> </name>
+     <description><![CDATA[ 
+
+	<a href='#{img_page}'><img alt='#{title}' 
+			width='240' src='#{m_url}' /></a>  
+	by #{owner} at lat:#{lat} lon:#{lon} 
+
+	]]></description>
+      <name> #{title} </name>
         <View>
           <longitude>#{lon}</longitude>
           <latitude>#{lat}</latitude>
@@ -46,7 +63,7 @@ puts "<Folder>
       <visibility>1</visibility>
       <styleUrl>root://styleMaps#default?iconId=0x307</styleUrl>
       <Style>
-       <icon>#{s_url}</icon>
+       <icon>#{m_url}</icon>
       </Style>
       <Point>
        <coordinates>#{lon},#{lat},0</coordinates>
