419fea9f92946a0e0b1bada1571483097ebc1581
danbri <danbri@55bc8a7a-491b-0410-89ea-f8cff589e9ef>
2006-10-21 14:35:14 +0000

    tweaked

---
+++ geo/photos/sp.rb
@@ -0,0 +1,10 @@
+#!/usr/bin/env ruby
+
+require 'fplanet'
+
+dir = "/home/danbri/spypixel.com/2006/kml/photos/"
+
+storeGroupKML("52240190192@N01",dir)# http://www.flickr.com/groups/iranian/pool/map
+storeGroupKML("43935391225@N01",dir) # http://www.flickr.com/groups/bsas/pool/map
+storeGroupKML("46594087@N00",dir) # http://www.flickr.com/groups/bristol/pool/map
+ 
