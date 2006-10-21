d7051c766bb8ad408b23301d3d3b451954d45e26
Dan Brickley <danbri@users.noreply.github.com>
2006-10-21 14:35:14 +0000

    tweaked

--- geo/photos/fplanet.rb
+++ geo/photos/fplanet.rb
@@ -1,5 +1,8 @@
 #!/usr/bin/env ruby
+
+
 require 'rubygems'
+Gem.path.push("/home/danbri/.gems")
 require 'json'
 
 # Ruby script to make KML for a Flickr group specified by its ID code
@@ -176,9 +179,7 @@ dir = "./examples/"
 
 
 
-storeGroupKML("52240190192@N01",dir)# http://www.flickr.com/groups/iranian/pool/map
-
-storeGroupKML("43935391225@N01",dir) # http://www.flickr.com/groups/bsas/pool/map
-
-storeGroupKML("46594087@N00",dir) # http://www.flickr.com/groups/bristol/pool/map
-
+#storeGroupKML("52240190192@N01",dir)# http://www.flickr.com/groups/iranian/pool/map
+#storeGroupKML("43935391225@N01",dir) # http://www.flickr.com/groups/bsas/pool/map
+#storeGroupKML("46594087@N00",dir) # http://www.flickr.com/groups/bristol/pool/map
+ 
