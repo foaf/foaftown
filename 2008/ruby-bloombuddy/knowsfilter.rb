0b4f828c20316e0905e8bce001c94df1d7a9639d
danbri <danbri@55bc8a7a-491b-0410-89ea-f8cff589e9ef>
2008-01-08 11:57:34 +0000

    tweaks

--- 2008/ruby-bloombuddy/knowsfilter.rb
+++ 2008/ruby-bloombuddy/knowsfilter.rb
@@ -7,31 +7,45 @@ require 'BloominSimple'
 require 'json'
 require 'digest/sha1'
 
+# we pass in a hasher block
 #bf = BloominSimple.new(1_000_000) { |item| Digest::SHA1.digest(item.downcase.strip).unpack("VVVV") }
-bf = BloominSimple.new(1000) { |item| Digest::SHA1.digest(item.downcase.strip).unpack("VVVV") }
+bf = BloominSimple.new(1000) do |item| Digest::SHA1.digest(item.downcase.strip).unpack("VVVV") end
+
+
+# see also http://ajax.suaccess.org/ruby/ruby-on-rails-converting-between-numeric-bases/
+# http://www.faqs.org/rfcs/rfc2045.html
+# perl: http://perldoc.perl.org/functions/pack.html
+#     b	A bit string (ascending bit order inside each byte, like vec()).
+# http://safari.oreilly.com/9780596529864/array_pack_directives
+# http://www.rubycentral.com/pickaxe/ref_c_array.html#Array.pack
+puts "############"
 
 js = `roqet -q bloomer.rq -r json`; # get contact list as prop/value pairs
 res = JSON.parse js
 res["results"]["bindings"].each do |contact|
     prop = contact['prop']['value']
     val = contact['val']['value']
-    # puts "Adding to bloom: #{prop} #{val} "
+     print "Adding: '#{prop} #{val}' ; "
     bf.add("#{prop} #{val}")
+    puts
 end
 
-#puts                bf.includes?("http://xmlns.com/foaf/0.1/mbox mailto:d.m.steer@lse.ac.uk")     # => true
-#puts                bf.includes?("http://xmlns.com/foaf/0.1/mbox mailto:santaclaus@npole.example.com")    # => false
-#puts                bf.includes?("http://xmlns.com/foaf/0.1/openid http://santa.example.com/")    # => false
+puts                bf.includes?("http://xmlns.com/foaf/0.1/mbox mailto:d.m.steer@lse.ac.uk")     # => true
+puts                bf.includes?("http://xmlns.com/foaf/0.1/mbox mailto:santaclaus@npole.example.com")    # => false
+puts                bf.includes?("http://xmlns.com/foaf/0.1/openid http://santa.example.com/")    # => false
  
 
 puts "Serializing to base 16: "
 a= eval "0b"+bf.bitfield.to_s()
 puts a.to_s(base=16)
 
+puts "a is "+a.to_s
+
 s = [a.to_s].pack("m")
 puts "Base64: #{s}"
 
-puts bf.hashnums
+#puts bf.hashnums
+puts bf.class
 
 #puts "Binary: "
 #a= eval "0b"+bf.bitfield.to_s()
--- 2008/ruby-bloombuddy/reader.rb
+++ 2008/ruby-bloombuddy/reader.rb
@@ -6,26 +6,29 @@ bf = BloominSimple.new(1000) { |item| Digest::SHA1.digest(item.downcase.strip).u
 t1= eval "0x8140a000048840080200000804800004802212010009026040802000000081042010400002004000008208008800000460812080000000000000010000008000010008000100001000000300400010108800440104a00280000409020a008800010000000800020420828284000004100010000000000004000400640"
 # Pad out the binary from the left if needed (is this right?)
 bs = t1.to_s(base=2)
-
-puts "Length of bs is : #{bs.length}"
-missing = 1000-bs.length
-puts "Missing #{missing} chars"
+puts "bits: #{bs}\n"
+puts "Length of bitstring is : #{bs.length}"
+missing = 1000-bs.length # should it be 1000 long?
+puts "Missing #{missing} chars; padding with leading 0"
 while (missing>0) 
  bs = "0#{bs}"
  missing -= 1
 end
 puts "Length of bs is now: #{bs.length}"
 
-puts "Binary0: "
+puts "\n\nBinary:\n "
 a= eval "0b"+bf.bitfield.to_s()
 puts a.to_s(base=2)
+print "\n"+a.to_s+"\n"
 i=0
 bs.scan(/./u) do |c| 
+    print ":" +c 
     bf.bitfield[i] = 1 if c=="1"
     bf.bitfield[i] = 0 if c=="0"
     i+=1
 end
 
+puts
 puts "Re-Serializing in base 16: "
 a= eval "0b"+bf.bitfield.to_s()
 puts a.to_s(base=16)
