333170a14fea9f53af352c5e55e008c0a5d6894c
danbri <danbri@55bc8a7a-491b-0410-89ea-f8cff589e9ef>
2008-06-26 16:12:29 +0000

    basic rdfizer for opensocial schema

---
+++ opensocial/vocab/extractOpensocial.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+#svn co http://opensocial-resources.googlecode.com/svn/spec/0.8/opensocial/
+
+java -cp js.jar org.mozilla.javascript.tools.shell.Main -f \
+     opensocial/opensocial.js \
+     -f opensocial/name.js -f opensocial/person.js -f opensocial/organization.js \
+	-f opensocial/address.js -f opensocial/name.js -f opensocial/address.js \
+	-f opensocial/activity.js -f opensocial/email.js \
+	-f opensocial/mediaitem.js -f opensocial/message.js -f opensocial/phone.js -f opensocial/url.js \
+	-f schemarama.js
+
---
+++ opensocial/vocab/readme.txt
@@ -0,0 +1,18 @@
+Some experiments with RDFizing opensocial
+
+-main code is schemarama.js
+-invoked from the .sh
+-requires .js from opensocial svn
+-generates .html and .rdf ; see ugly Makefile
+
+js.jar is Mozilla's Rhino, a java javascript interpreter.
+See http://www.mozilla.org/rhino/
+
+opensocial/*.js comes from the OpenSocial SVN, copied for now from 
+ http://opensocial-resources.googlecode.com/svn/spec/0.8/opensocia
+l/
+
+Dan 
+
+--
+http://danbri.org/
