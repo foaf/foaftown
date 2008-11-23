1400e7ecaa06e1e747e5667b082358ee93515525
Dan Brickley <danbri@users.noreply.github.com>
2008-11-23 11:16:32 +0000

    wip

---
+++ 2008/rdfa/oilit/new/rdfaize.rb
@@ -0,0 +1,33 @@
+#!/usr/bin/env ruby
+
+# Reads a file, outputs a basic RDFa-ization of it
+# Dan Brickley <http://danbri.org/>
+
+oiltype="oilit:OilIndustryOrganization"
+oiltype="oilit:OilIndustryProduct"
+
+file = ARGV.shift
+
+if file==nil 
+  warn("no file specified")
+  exit(1)
+end
+
+STDERR.puts "Reading file: #{file}"
+
+f = File.open(file,'r')
+if f == nil
+  warn("No file found: #{file} ")
+  exit(1)
+end
+
+f.each_line do |line|
+ line.gsub!(/<a href="([^"]*)">([^<]*)<\/a>\s*<br\s*\/>/) do  
+    "<div typeof='foaf:Organization #{oiltype}'>"+
+    "<a rev='foaf:primaryTopic' about='#{$1}#itself' "+
+    "href='#{$1}' property='foaf:name' >#{$2}</a></div>\n"
+ end
+
+ puts line
+
+end
