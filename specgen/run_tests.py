dace5e789a325cf3f0c8e1360b6617fcf2407ac1
Dan Brickley <danbri@users.noreply.github.com>
2008-12-11 17:04:56 +0000

    five tests defined and parsing.

--- specgen/run_tests.py
+++ specgen/run_tests.py
@@ -3,6 +3,7 @@
 # trying to test using ...
 # http://agiletesting.blogspot.com/2005/01/python-unit-testing-part-1-unittest.html 
 # but getting errors. --danbri
+# http://docs.python.org/library/unittest.html
  
 import libvocab
 from libvocab import Vocab
@@ -28,47 +29,65 @@ from rdflib.sparql.graphPattern import GraphPattern
 from rdflib.sparql.bison import Parse 
 from rdflib.sparql import Query 
 
-
 import unittest
 
 class testSpecgen(unittest.TestCase):
 
   """a test class for Specgen"""
 
+  def setUp(self):
+    """set up data used in the tests. Called  before each test function execution."""
 
+  def testFOAFns(self):
+    foaf_spec = Vocab('examples/foaf/index.rdf')
+    foaf_spec.index()
+    foaf_spec.uri = FOAF
+    print "FOAF should be "+FOAF
+    self.assertEqual(str(foaf_spec.uri), 'http://xmlns.com/foaf/0.1/')
 
-def setUp(self):
-  """set up data used in the tests. Called  before each test function execution."""
+  def testSIOCns(self):
+    sioc_spec = Vocab('examples/sioc/sioc.rdf')
+    sioc_spec.index()
+    sioc_spec.uri = str(SIOC)
+    self.assertEqual(sioc_spec.uri, 'http://rdfs.org/sioc/ns#')
 
-def testFOAFns(self):
-  foaf_spec = Vocab('examples/foaf/index.rdf')
-  foaf_spec.index()
-  foaf_spec.uri = FOAF
+  def testDOAPWrongns(self):
+    doap_spec = Vocab('examples/doap/doap-en.rdf')
+    doap_spec.index()
+    doap_spec.uri = str(DOAP)
+    self.assertNotEqual(doap_spec.uri, 'http://example.com/DELIBERATE_MISTAKE_HERE')
 
-  self.assertEqual(foaf_spec.uri, 'http://xmlns.com/foaf/0.1/')
+  def testDOAPns(self):
+    doap_spec = Vocab('examples/doap/doap-en.rdf')
+    doap_spec.index()
+    doap_spec.uri = str(DOAP)
+    self.assertEqual(doap_spec.uri, 'http://usefulinc.com/ns/doap#')
 
-
-def testSIOCns(self):
-  sioc_spec = Vocab('examples/sioc/sioc.rdf')
-  sioc_spec.index()
-  sioc_spec.uri = SIOC
-
-def testDOAPns(self):
-  doap_spec = Vocab('examples/doap/doap-en.rdf')
-  doap_spec.index()
-  doap_spec.uri = DOAP
-  self.assertEqual(doap_spec.uri, 'http://example.com/nothere')
-
+  # I'd like this to be failing. need to lookup property set/getter stuff in .py
+  # then we can trap calls to get/set .uri and str'ify them?
+  # 
+  def testCanUseNonStrURI(self):
+    doap_spec = Vocab('examples/doap/doap-en.rdf')
+    doap_spec.index()
+    doap_spec.uri = Namespace('http://usefulinc.com/ns/doap#')  # likely a common mistake
+    self.assertNotEqual(doap_spec.uri, 'http://usefulinc.com/ns/doap#')
 
 def suite():
-    suite = unittest.TestSuite()
-    suite.addTest(unittest.makeSuite(testSpecgen))
-    return suite
+      suite = unittest.TestSuite()
+      suite.addTest(unittest.makeSuite(testSpecgen))
+      return suite
 
 if __name__ == '__main__':
-
+    print "Running tests..."
     suiteFew = unittest.TestSuite()
     suiteFew.addTest(testSpecgen("testFOAFns"))
+    suiteFew.addTest(testSpecgen("testSIOCns"))
+    suiteFew.addTest(testSpecgen("testDOAPns"))
+
+#    unittest.TextTestRunner(verbosity=2).run(suiteFew)
+
+    unittest.TextTestRunner(verbosity=2).run(suite())
+
 
 # http://agiletesting.blogspot.com/2005/01/python-unit-testing-part-1-unittest.html g = foafspec.graph 
 #q= 'SELECT ?x ?l ?c ?type WHERE { ?x rdfs:label ?l . ?x rdfs:comment ?c . ?x a ?type . FILTER (?type = owl:ObjectProperty || ?type = owl:DatatypeProperty || ?type = rdf:Property || ?type = owl:FunctionalProperty || ?type = owl:InverseFunctionalProperty) } '
@@ -77,5 +96,3 @@ if __name__ == '__main__':
 #for (term, label, comment) in relations:
 #        p = Property(term)
 #        print "property: "+str(p) + "label: "+str(label)+ " comment: "+comment
-
-#  """set up data used in the tests. Called  before each test function execution."""
