c92f45891b06236a1d6d6bcfd84bc33b1f304b6f
Dan Brickley <danbri@users.noreply.github.com>
2009-02-17 20:09:26 +0000

    prettied

--- 2009/skosdex/src/testskos.rb
+++ 2009/skosdex/src/testskos.rb
@@ -1,10 +1,10 @@
 #!/usr/bin/env jruby 
 
-require 'java'
-include Java
-
 require "src/jena_skos"
 
-SKOS.new("http://norman.walsh.name/knows/taxonomy").concepts.each_pair do |k,v|
-  puts "SKOS Concept: #{k} prefLabel: #{v.prefLabel}"
+SKOS.new( "http://norman.walsh.name/knows/taxonomy").read(
+	  "http://www.wasab.dk/morten/blog/archives/author/mortenf/skos.rdf").concepts.each_pair do |url,c|
+
+  puts "SKOS Concept: #{url} prefLabel: #{c.prefLabel}"
+
 end
