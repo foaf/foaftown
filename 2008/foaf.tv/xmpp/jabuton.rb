fa370654c7baa0114af88b65971e6df6d6f3a4e1
Dan Brickley <danbri@users.noreply.github.com>
2009-10-11 14:14:29 +0000

    switchboarded

---
+++ 2008/foaf.tv/xmpp/jabuton.rb
@@ -0,0 +1,25 @@
+1#!/usr/bin/env ruby -rubygems
+
+require 'switchboard'
+
+switchboard = Switchboard::Client.new
+switchboard.plug!(AutoAcceptJack, NotifyJack)
+
+switchboard.on_message do |message|
+  txt = message.body.to_s
+  puts "Message body was: '#{txt}'"
+  if (txt =~ /pause/)
+    puts "replied with pause toggle."
+    stream.send("toggled pause")
+  else
+    stream.send(message.answer)
+  end
+end
+
+switchboard.on_iq do |message|
+  p "Got message iq: "
+  p message
+end
+
+
+switchboard.run!
