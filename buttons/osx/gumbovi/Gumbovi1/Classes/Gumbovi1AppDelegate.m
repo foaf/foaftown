2983fac2c5f70c803c559d52f2a6f2049fc0d2e7
Dan Brickley <danbri@users.noreply.github.com>
2009-11-07 22:20:13 +0000

    fresh repo dir

--- buttons/osx/gumbovi/Gumbovi1/Classes/Gumbovi1AppDelegate.m
+++ buttons/osx/gumbovi/Gumbovi1/Classes/Gumbovi1AppDelegate.m
@@ -3,17 +3,29 @@
 //  Gumbovi1
 //
 //  Created by Dan Brickley on 11/7/09.
-//  Copyright __MyCompanyName__ 2009. All rights reserved.
-//
 
-#import "Gumbovi1AppDelegate.h"
+//
+// thanks to Jens Finkhäuser and Daniel Salber for Obj-C advice
+// XMPP smarts live in this zone...
+
+// jens: 'well, the idea is that firstviewcontroller controls the tab contents, app delegate controls tabs.
+// I suspect I'd hook most of what you've talked about up in firstviewcontroller'
+//
+//
+// send xmpp chats with: (void)sendMessage:(NSString *)message toJID:(XMPPJID *)jid;
+
 #import "XMPP.h"
+#import "FirstViewController.h"
+#import "Gumbovi1AppDelegate.h"
+
+#import <Foundation/NSString.h>
 
 @implementation Gumbovi1AppDelegate
 
-@synthesize xmppclient;
+@synthesize xmppClient;
 @synthesize window;
 @synthesize tabBarController;
+
 
 - (void)applicationDidFinishLaunching:(UIApplication *)application {
     
@@ -22,10 +34,20 @@
 	
     xmppClient = [[XMPPClient alloc] init];
 	[xmppClient addDelegate:self];
-	[xmppClient setDomain:@"foaf.tv"];
+	
+	// connect as buttons 
+//	[xmppClient setDomain:@"foaf.tv"];
+//	[xmppClient setPort:5222];	
+//	[xmppClient setMyJID:[XMPPJID jidWithString:@"buttons@foaf.tv/iPhoneTest"]];
+//	[xmppClient setPassword:@"gargonza"];	
+	
+	
+	// or connect as bob?
+	[xmppClient setDomain:@"talk.google.com"];
 	[xmppClient setPort:5222];	
-	[xmppClient setMyJID:[XMPPJID jidWithString:@"buttons@foaf.tv/iPhoneTest"]];
+	[xmppClient setMyJID:[XMPPJID jidWithString:@"bob.notube@gmail.com/iPhoneTest"]];
 	[xmppClient setPassword:@"gargonza"];	
+	
 	[xmppClient setAutoLogin:YES];
 	[xmppClient setAllowsPlaintextAuth:NO];
 	[xmppClient setAutoPresence:YES];
@@ -59,40 +81,53 @@
 - (void)sendPLUS:(NSObject *)button
 {
 	NSLog(@"SENDING PLUS%@", button);
+	NSString *msg = @"PLUS event.";
+    XMPPJID *aJid = [XMPPJID jidWithString:@"buttons@foaf.tv"];
+	[ self.xmppClient sendMessage:msg toJID:aJid ] ;
 }
 
 - (void)sendLEFT:(NSObject *)button
 {
 	NSLog(@"SENDING LEFT %@", button);
+	NSString *msg = @"LEFT event.";
+    XMPPJID *aJid = [XMPPJID jidWithString:@"buttons@foaf.tv"];
+	[ self.xmppClient sendMessage:msg toJID:aJid ] ;
 }
 
 - (void)sendRIGH:(NSObject *)button
 {
 	NSLog(@"SENDING RIGH %@", button);
+	NSString *msg = @"RIGH event.";
+    XMPPJID *aJid = [XMPPJID jidWithString:@"buttons@foaf.tv"];
+	[ self.xmppClient sendMessage:msg toJID:aJid ] ;
 }
 
 - (void)sendMINU:(NSObject *)button
 {
 	NSLog(@"SENDING MINU %@", button);
+	NSString *msg = @"MINU event.";
+    XMPPJID *aJid = [XMPPJID jidWithString:@"buttons@foaf.tv"];
+	[ self.xmppClient sendMessage:msg toJID:aJid ] ;
 }
 
 - (void)sendPLPZ:(NSObject *)button
 {
 	NSLog(@"SENDING PLPZ %@", button);
-//	- (void)sendMessage:(NSString *)message toJID:(XMPPJID *)jid;
-    // create msg and aJid
-
-	NSString *msg = @"Hello alice, we got a PLPZ event.";
-    XMPPJID *aJid = [XMPPJID jidWithString:@"alice.notube@gmail.com"];
-	[ self.xmppclient sendMessage:msg toJID:aJid ] ;
+	NSString *msg = @"PLPZ event.";
+    XMPPJID *aJid = [XMPPJID jidWithString:@"buttons@foaf.tv"];
+	[ self.xmppClient sendMessage:msg toJID:aJid ] ;
 	NSLog(@"Sent msg %@", msg);
 	NSLog(@"To jid %@", aJid);
+	NSLog(@" XMPP CLient: %@", self.xmppClient );
 
 }
 
 - (void)sendMENU:(NSObject *)button
 {
 	NSLog(@"SENDING MENU %@", button);
+	NSString *msg = @"MENU event.";
+    XMPPJID *aJid = [XMPPJID jidWithString:@"buttons@foaf.tv"];
+	[ self.xmppClient sendMessage:msg toJID:aJid ] ;
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -181,6 +216,12 @@
 	NSLog(@"==============================================================");
 	NSLog(@"iPhoneXMPPAppDelegate: xmppClient:didReceiveMessage: %@", message);
 	NSLog(@"==============================================================");
+	/// tabBarDelegate window.output.text = @"Got message!" ;
+	//tabBarController.selectedViewController.output.text = @"Got message!";
+	FirstViewController * fvc = (FirstViewController *) tabBarController.selectedViewController;
+    NSString *m = [ message elementForName:@"body"  ] ;
+	fvc.output.text =  m.description;
+
 }
 
 
