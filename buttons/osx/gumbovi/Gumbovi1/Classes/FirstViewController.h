7dea75a5a33a8aa8aa35fea89b954abd9d406d46
Dan Brickley <danbri@users.noreply.github.com>
2009-11-07 18:21:38 +0000

    ui shows, xmpp connects, what's not to like?

--- buttons/osx/gumbovi/Gumbovi1/Classes/FirstViewController.h
+++ buttons/osx/gumbovi/Gumbovi1/Classes/FirstViewController.h
@@ -11,6 +11,34 @@
 
 @interface FirstViewController : UIViewController {
 
+	IBOutlet UITextField *userid;
+	IBOutlet UITextField *password;
+
+	IBOutlet UIButton *plus;
+	IBOutlet UIButton *minu;
+	IBOutlet UIButton *left;
+	IBOutlet UIButton *righ;
+	IBOutlet UIButton *plpz;
+	IBOutlet UIButton *men;
+
+	IBOutlet UITextField *output;
+	
 }
+
+	@property (nonatomic, retain) IBOutlet UITextField *userid;
+	@property (nonatomic, retain) IBOutlet UITextField *password;
+	
+	// remote tab
+	@property (nonatomic, retain) IBOutlet UIButton *plus;
+	@property (nonatomic, retain) IBOutlet UIButton *minu;
+	@property (nonatomic, retain) IBOutlet UIButton *left;
+	@property (nonatomic, retain) IBOutlet UIButton *righ;
+	@property (nonatomic, retain) IBOutlet UIButton *plpz;
+	@property (nonatomic, retain) IBOutlet UIButton *menu;
+	//
+	@property (nonatomic, retain) IBOutlet UITextView *output;
+	
+
+	- (IBAction) updateText:(id) sender;
 
 @end
