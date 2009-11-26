914d0170f70b163c8e27f55bcf550c36bab13e94
libby <libbymiller@users.noreply.github.com>
2009-11-26 14:30:39 +0000

    updated updatelivefoaf.sh to reflect new syntax

--- specgen/updatelivefoaf.sh
+++ specgen/updatelivefoaf.sh
@@ -3,4 +3,5 @@
 # this assumes template.html in main dir, and drops a _tmp... file into that dir as a candidate index.html
 # also assumes http://svn.foaf-project.org/foaf/trunk/xmlns.com/ checked out in parallel filetree
 
-./specgen5.py ../../xmlns.com/htdocs/foaf/spec/ http://xmlns.com/foaf/0.1/ foaf 
+./specgen5.py --indir=../../xmlns.com/htdocs/foaf/spec/ --ns=http://xmlns.com/foaf/0.1/ --prefix=foaf
+
