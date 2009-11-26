b0bb8d0870765abd6c3b336a9f9013958b438bcb
libby <libby@55bc8a7a-491b-0410-89ea-f8cff589e9ef>
2009-11-26 14:12:41 +0000

    Basic getops now used. No outfile or outdir yet

--- specgen/updatelivefoaf.sh
+++ specgen/updatelivefoaf.sh
@@ -3,4 +3,4 @@
 # this assumes template.html in main dir, and drops a _tmp... file into that dir as a candidate index.html
 # also assumes http://svn.foaf-project.org/foaf/trunk/xmlns.com/ checked out in parallel filetree
 
-./specgen5.py ../../foaf/trunk/xmlns.com/htdocs/foaf/spec/ http://xmlns.com/foaf/0.1/ foaf 
+./specgen5.py ../../xmlns.com/htdocs/foaf/spec/ http://xmlns.com/foaf/0.1/ foaf 
