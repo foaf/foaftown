ac1cb03d89eb4f02b182ed353bcf217a9427ddbf
Dan Brickley <danbri@users.noreply.github.com>
2009-12-29 11:35:03 +0000

    tidy

--- 2010/wordpress/f2f/f2f.php
+++ 2010/wordpress/f2f/f2f.php
@@ -16,14 +16,6 @@ Version: 0.2
 Author URI: http://danbri.org/
 */
 
-// Reading ...
-// http://ditio.net/2007/08/09/how-to-create-wordpress-plugin-from-a-scratch/
-// http://phpxref.ftwr.co.uk/wordpress/nav.html?_functions/index.html
-// http://codex.wordpress.org/Adding_Administration_Menus
-// http://planetozh.com/blog/2008/04/how-to-load-javascript-with-your-wordpress-plugin/
-// http://codex.wordpress.org/Function_Reference/get_currentuserinfo
-// Goals: http://lists.foaf-project.org/pipermail/foaf-dev/2009-December/009962.html
-
 function f2f_openid_list_rdfa() {
   $req = $_SERVER['REQUEST_URI'];
 
@@ -35,23 +27,14 @@ function f2f_openid_list_rdfa() {
   // Only do anything heavy if we're in the configured pages
   if (preg_match("/$f2f_path/i", $req)) {
    global $wpdb; 
-   $css = "position: absolute;
-		top: 450px;
-		margin: 0;
-		padding: 0;
-		right: 450px;
-		font-size: 12px;
-		background: #eeeeee; ";
-
+   $css = "position: absolute; top: 450px; margin: 0; padding: 0; right: 450px; font-size: 12px; background: #eeeeee; ";
    $ret = "<!-- Page is $req  -->";
    $q = "SELECT distinct url FROM wp_comments, wp_openid_identities WHERE wp_openid_identities.user_id=wp_comments.user_id AND comment_approved='1' ORDER BY url;";
    $urls = $wpdb->get_results($q);
-
    $ret .= "<div about='#!aCommentApprovedTrustGroup' typeof='foaf:Group'>\n";
    $blogurl = get_bloginfo('url'); // http://codex.wordpress.org/Function_Reference/get_bloginfo
    $blogname = get_bloginfo('name');
    $ret .= "<span rel='foaf:maker'>Comment acccept list for <a typeof='foaf:Agent' rel='foaf:weblog foaf:account' href='".$blogurl."/'>".$blogurl."</a></span>";
-
    $ret .=  "<ul style='". $css . "'>\n";
    foreach($urls as $openid)   
     {
@@ -74,28 +57,5 @@ function f2f_main() {
 }
 
 add_action('wp_footer', 'f2f_main');
-
-
-
-
-
- // ------------------------------------------------------------------
- // Add all your sections, fields and settings during admin_init
- // ------------------------------------------------------------------
- //
- 
- function eg_settings_api_init() {
-     // Add the section to reading settings so we can add our fields to it
-     add_settings_section('eg_setting_section', 'Example settings section in reading', 'eg_setting_section_callback_function', 'reading');
-     
-     // Add the field with the names and function to use for our new settings, put it in our new section
-     add_settings_field('eg_setting_name', 'Example setting Name', 'eg_setting_callback_function', 'reading', 'eg_setting_section');
-     
-     // Register our setting so that $_POST handling is done for us and our callback function just has to echo the <input>
-     register_setting('reading','eg_setting_name');
- }// eg_settings_api_init()
- 
- add_action('admin_init', 'eg_settings_api_init');
- 
 
 ?>
