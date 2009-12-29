d862ede271664580e88171e6429ce8c6a0a76750
Dan Brickley <danbri@users.noreply.github.com>
2009-12-29 15:18:14 +0000

    tidied

--- 2010/wordpress/aggtest.py
+++ 2010/wordpress/aggtest.py
@@ -9,18 +9,6 @@ g = ConjunctiveGraph()
 g.parse("http://inkdroid.org/journal/network", format='rdfa', lax=True)
 g.parse("http://danbri.org/words/network", format='rdfa', lax=True)
 
-#res = g.query("SELECT ?g ?s ?p ?o WHERE { GRAPH ?g { ?s ?p ?o } } ")
-#for src, sub, pred, obj in res:
-#    print src, sub, pred, obj
-
-
-q1="""PREFIX foaf: <http://xmlns.com/foaf/0.1/>
-SELECT ?src1 ?src2 ?x WHERE {
- GRAPH ?src1 { ?gr1 foaf:member ?x . } 
- GRAPH ?src2 { ?gr2 foaf:member ?x . }  
- FILTER ( ?src1 != ?src2 ) 
- }"""
-
 q1="""PREFIX foaf: <http://xmlns.com/foaf/0.1/>
     SELECT ?src1 ?src2 ?x 
     WHERE {
@@ -29,6 +17,5 @@ q1="""PREFIX foaf: <http://xmlns.com/foaf/0.1/>
         FILTER ( ?src1 != ?src2 )
     }"""
 
-print q1
 for src1, src2, x in  g.query(q1):
     print src1, src2, x
