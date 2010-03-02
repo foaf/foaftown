cbadfd892abdc308952813e557f48a89ea11378b
danbri <danbri@55bc8a7a-491b-0410-89ea-f8cff589e9ef>
2010-03-02 14:51:38 +0000

    We have a tableview controller, and the roster somewhat hooked up to it. rows are JIDs. It does not refresh when QR adds to it.

---
+++ buttons/osx/gumbovi/iphone/Gumbovi1/Gumbovi1/Classes/LinkedTVRosterTableController.h
@@ -0,0 +1,17 @@
+//
+//  LinkedTVRosterTableController.h
+//  Gumbovi1
+//
+//  Created by Dan Brickley on 3/2/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface LinkedTVRosterTableController : UITableViewController {
+	IBOutlet UITableView *roster_view;
+}
+@property (nonatomic, retain) IBOutlet UITableView *roster_view;
+
+@end
