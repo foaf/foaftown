80a48e2eef87fb5ed9f8e4402e16d5840d6dc42a
Dan Brickley <danbri@users.noreply.github.com>
2010-03-25 18:17:09 +0000

    added buttonDevices

---
+++ buttons/osx/gumbovi/iphone/Gumbovi1/Gumbovi1/Classes/ButtonDeviceList.m
@@ -0,0 +1,22 @@
+//
+//  ButtonDeviceList.m
+//  Gumbovi1
+//
+//  Created by Dan Brickley on 25/03/2010.
+//  Copyright 2010 FOAF. All rights reserved.
+//
+
+#import "ButtonDeviceList.h"
+
+
+@implementation ButtonDeviceList
+
+- (void)dealloc {
+	
+	//[anything_we_use release];
+	
+    [super dealloc];
+}
+
+
+@end
