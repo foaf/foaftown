f2cb71127d803d62bc2ccd3c3e82d2b8356b0f91
Dan Brickley <danbri@users.noreply.github.com>
2010-05-03 22:13:03 +0000

    messy

--- 2008/foaf.tv/xmpp/jabutton.rb
+++ 2008/foaf.tv/xmpp/jabutton.rb
@@ -2,14 +2,81 @@
 require 'appscript'
 include Appscript
 require 'xmpp4r'
+require 'xmpp4r/discovery'
+require 'xmpp4r/caps/c'
+require 'digest/sha1'
+require 'base64'
+require 'xmpp4r/tune'
+require 'xmpp4r/tune/tune'
+require 'xmpp4r/tune/helper/helper'
+require 'xmpp4r/caps/helper/generator'
+require 'xmpp4r/discovery/iq/discoinfo'
+require 'xmpp4r/discovery/iq/discoitems'
+require 'xmpp4r/version/helper/simpleresponder'
+require 'base64'
+require 'md5'
+# hmm /usr/lib/ruby//user-gems/1.8/gems/xmpp4r-0.5/lib/xmpp4r/
+
 include Jabber
-# config: put the following 3 lines (no '#' marks and a real password!) into ~/.switchboardrc
+Jabber::debug = true
 
 require 'switchboard'
 switchboard = Switchboard::Client.new
-#switchboard.plug!(AutoAcceptJack, NotifyJack)
+
+# caps_helper=Jabber::Caps::Helper(switchboard,  [Jabber::Discovery::Identity.new('client',nil,'bot')], [Jabber::Discovery::Feature.new('http://buttons.foaf.tv/')] )
+
 switchboard.plug!(AutoAcceptJack)
 
+switchboard.on_iq do |message|
+  p "Got IQ message "						# Jabber::IqQuery < Jabber::XMPPElement < REXML::Element
+
+  itunes = app('iTunes.app')
+  track = itunes.current_track
+  p track                     # <OSA::Itunes::FileTrack:0x1495e20>
+  p track.name                # "Over The Rainbow"
+  p track.artist              # "Keith Jarrett"
+  p track.duration            # 362.368988037109
+  p track.date_added.to_s     # "2006-06-30"
+  body = "Currently playing #{ track.name } by #{ track.artist }, duration: #{  track.duration } "
+
+  if (message.query.namespace=='http://buttons.foaf.tv/') 
+    b = message.query.first_element('button').text.to_s
+    puts "Got a buttons message! #{b}"
+    if (b == 'PLPZ')
+      puts "Got PLPZ: Playing and/or pausing or vice-versa."
+      state = itunes.player_state.get.to_s
+      puts "State was: #{state}"
+      if state =~ /playing/
+        itunes.pause
+        puts "Paused."
+      else 
+        itunes.play
+        puts "Play."
+      end
+    end
+    if (b == '')
+    end
+  end
+end
+
+
+switchboard.run!
+
+#todo - send presence?
+# http://stdlib.rubyonrails.org/libdoc/rexml/rdoc/index.html
+
+
+# migrating from xmpp api
+#    m = Message::new(to, body).set_type(:normal).set_id('1').set_subject(subject)
+#    cl.send m
+
+#source = "<a xmlns:x='foo' xmlns:y='bar'><x:b id='1'/><y:b id='2'/></a>"
+#doc = Document.new source
+#doc.elements["/a/x:b"].attributes["id"]	                     # -> '1'
+#XPath.first(doc, "/a/m:b", {"m"=>"bar"}).attributes["id"]   # -> '2'
+#doc.elements["//x:b"].prefix                                # -> 'x'
+#doc.elements["//x:b"].namespace	                             # -> 'foo'
+#XPath.first(doc, "//m:b", {"m"=>"bar"}).prefix  
 switchboard.on_message do |message|
   txt = message.body.to_s
   puts "Message body was: '#{txt}'"
@@ -52,38 +119,4 @@ switchboard.on_message do |message|
    #  stream.send(message.answer)
   end
 end
-
-# xmpp4r Jabber:Iq instances arriving:
-#
-switchboard.on_iq do |message|
-  p "Got message iq: "
-			# Jabber::IqQuery < Jabber::XMPPElement < REXML::Element
-  if (message.query.namespace=='http://buttons.foaf.tv/') 
-    puts "Got a buttons message!"
-    b = message.query.first_element('button').text.to_s
-    if (b == 'PLPZ')
-      puts "Got PLPZ: Playing and/or pausing or vice-versa."
-
-    end
-    
-  end
-  
-end
-
-switchboard.run!
-
-#todo - send presence?
-# http://stdlib.rubyonrails.org/libdoc/rexml/rdoc/index.html
-
-
-# migrating from xmpp api
-#    m = Message::new(to, body).set_type(:normal).set_id('1').set_subject(subject)
-#    cl.send m
-
-#source = "<a xmlns:x='foo' xmlns:y='bar'><x:b id='1'/><y:b id='2'/></a>"
-#doc = Document.new source
-#doc.elements["/a/x:b"].attributes["id"]	                     # -> '1'
-#XPath.first(doc, "/a/m:b", {"m"=>"bar"}).attributes["id"]   # -> '2'
-#doc.elements["//x:b"].prefix                                # -> 'x'
-#doc.elements["//x:b"].namespace	                             # -> 'foo'
-#XPath.first(doc, "//m:b", {"m"=>"bar"}).prefix  
+# config: put the following 3 lines (no '#' marks and a real password!) into ~/.switchboardrc
