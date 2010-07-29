01e3066f6951134a6cbac0729d31d33d66fd3ac3
Dan Brickley <danbri@users.noreply.github.com>
2010-07-29 20:31:42 +0000

    tidying

--- specgen/myfoaf.sh
+++ specgen/myfoaf.sh
@@ -3,13 +3,16 @@
 # this assumes template.html in main dir, and drops a _tmp... file into that dir as a candidate index.html
 # also assumes http://svn.foaf-project.org/foaf/trunk/xmlns.com/ checked out in parallel filetree
 
-./specgen5.py --indir=../../foaf/trunk/xmlns.com/htdocs/foaf/ \
+export XMLCOM=../xmlns.com
+# XMLCOM../../foaf/trunk/xmlns.com
+
+./specgen5.py --indir=$XMLCOM/htdocs/foaf/ \
 --ns=http://xmlns.com/foaf/0.1/ \
 --prefix=foaf \
---templatedir=../../foaf/trunk/xmlns.com/htdocs/foaf/spec/ \
---indexrdfdir=../../foaf/trunk/xmlns.com/htdocs/foaf/spec/ \
---outfile=20100101.html \
---outdir=../../foaf/trunk/xmlns.com/htdocs/foaf/spec/ 
+--templatedir=$XMLCOM/htdocs/foaf/spec/ \
+--indexrdfdir=$XMLCOM/htdocs/foaf/spec/ \
+--outfile=20100726.html \
+--outdir=$XMLCOM/htdocs/foaf/spec/ 
 
 
 #--outfile=_live_editors_edition.html \
