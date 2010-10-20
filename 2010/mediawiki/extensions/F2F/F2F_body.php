53c4fda6a49e72f090407d1fc6edf028ffa56642
Dan Brickley <danbri@users.noreply.github.com>
2010-10-20 10:29:26 +0000

    fixed a duff span, added link to announce and thx to tobyink

--- 2010/mediawiki/extensions/F2F/F2F_body.php
+++ 2010/mediawiki/extensions/F2F/F2F_body.php
@@ -6,11 +6,15 @@ class F2F extends SpecialPage
 # Author: Dan Brickley <danbri@danbri.org>
 # http://wiki.foaf-project.org/w/F2FMediawikiPlugin
 #
+# see initial announcement:
+# http://lists.foaf-project.org/pipermail/foaf-dev/2010-October/010447.html
+#
 # nearby useful links:
 # http://inspector.sindice.com/inspect?url=http://wiki.foaf-project.org/w/Special:F2F&content=
 # see also http://www.mediawiki.org/wiki/Manual:$wgOut
 #
-# thanks: mhausenblas, presbrey
+# thanks: mhausenblas, presbrey, tobyink
+
 
 # Some/all of these in LocalSettings.php are needed (todo - clarify)
 #$wgHTML5 = true;
@@ -79,7 +83,8 @@ class F2F extends SpecialPage
 	        $wgOut->addHTML("<li rel='foaf:member'>\n");
                 $wgOut->addHTML("<div typeof='foaf:Agent'>");
                 $wgOut->addHTML("<a href='" . $row['uoi_openid']. "' rel='foaf:openid foaf:account' property='foaf:name'>");
-                $wgOut->addHTML(  "<span>" . $row['user_name'] . "</span>");
+//                $wgOut->addHTML(  "<span>" . $row['user_name'] . "</span>");
+                $wgOut->addHTML( $row['user_name'] );
     		$wgOut->addHTML("</a>\n");
 		$wgOut->addHTML("</div>\n");
 		$wgOut->addHTML("</li>\n");
@@ -94,3 +99,11 @@ class F2F extends SpecialPage
         $db->freeResult($res);
     }
 }
+
+# [12:18]  <tobyink> danbri: see http://check.rdfa.info/check?url=http%3A%2F%2Fwiki.foaf-project.org%2Fw%2FSpecial%3AF2F&version=1.0
+# [12:19]  <tobyink> versus http://check.rdfa.info/check?url=http%3A%2F%2Fwiki.foaf-project.org%2Fw%2FSpecial%3AF2F&version=1.1
+# [12:19]  <tobyink> If you want RDFa 1.0 compatibility either drop the redundant <span> elements, or add datatype="" to the <a> elements.
+# [12:27]  <logger> See http://chatlogs.planetrdf.com/swig/2010-10-20#T10-27-47
+
+
+
