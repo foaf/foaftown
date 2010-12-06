8c9939ad0ea6c0aa614d5c91f415f2c94fe8d7b4
danbri <danbri@55bc8a7a-491b-0410-89ea-f8cff589e9ef>
2010-12-06 16:51:30 +0000

    all

--- 2010/prov/graphit.pl
+++ 2010/prov/graphit.pl
@@ -4,10 +4,18 @@ use RDF::RDFa::Parser;
 use RDF::Trine;
 use constant { TRUE  => 1, FALSE => 0,};
 
-my $url = "file:surf2.html";
-my $p = RDF::RDFa::Parser->new_from_url(
-	$url,
-	RDF::RDFa::Parser::Config->new(
+# Note, CPAN installation of the RDF stuff may fail UNLESS you
+# install Test::Simple via CPAN first. Known problem with RDF::Trine.
+
+
+my $file = "surf3.html";
+my $base_uri = "http://example.com/rdfatests#";
+my $markup = '';
+open(IN,$file) || die "No file to read, $file";
+while(<IN>) {$markup .= $_;}
+my $p = RDF::RDFa::Parser->new(
+	$markup, $base_uri,
+	RDF::RDFa::Parser::Config->new( 
 		RDF::RDFa::Parser::Config->HOST_XHTML,
 		RDF::RDFa::Parser::Config->RDFA_11,
 		graph => TRUE,
